81a0ed5658c2bc7c088750c4be814007b30a2262
Schlobban <roangark@hotmail.se>
2016-08-29 15:55:17 +0200

    Rain effect added

---
+++ Assets/OrderLayers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrderLayers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+	}
+}
